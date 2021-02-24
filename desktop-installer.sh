98a57bcc938b332cf2451607977f74b9fc84aece
Abdon Morales <abdon.morales13_2022@outlook.com>
2021-02-23 20:49:28 -0600

    Updated script -- In testing (#2357)

--- desktop-installer.sh
+++ desktop-installer.sh
@@ -1,1 +1,7 @@
-pacman -S gnome hsetroot
+echo "Installing base desktop"
+# Installing base desktop environment which is cinnamon
+pacman -S hsetroot cinnamon
+# Install the desktop wallpaper
+echo "Installing Sun Desktop and wallpaper"
+cd ${HOME}/Wallpapers
+hsetroot -cover Sun-wallpaper.png
