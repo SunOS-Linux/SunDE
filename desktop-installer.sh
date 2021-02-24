f2ba98ec1f7aa06bd8230774bb1ebab398a7b21f
Abdon Morales <abdon.morales13_2022@outlook.com>
2021-02-23 20:58:15 -0600

    Update desktop-installer.sh

--- desktop-installer.sh
+++ desktop-installer.sh
@@ -1,6 +1,7 @@
 echo "Installing base desktop"
 # Installing base desktop environment which is cinnamon
 pacman -S hsetroot cinnamon
+pacman -Rs gnome
 # Install the desktop wallpaper
 echo "Installing Sun Desktop and wallpaper"
 cd ${HOME}/Wallpapers
